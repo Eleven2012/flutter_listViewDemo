d91aa9a52ffdefd55c2094f4dee1e04ba1916732
eleven 2011 <282889543@qq.com>
2020-05-15 21:09:31 +0800

    new layout modify

--- flutter_layoutdemo/lib/layout_demo.dart
+++ flutter_layoutdemo/lib/layout_demo.dart
@@ -4,7 +4,44 @@ class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      color: Colors.white,
+      alignment: Alignment(0.0, 0.0),
+      child: RowDemo()
+    );
+  }
+  
+
+}
+
+
+class RowDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            child:Text('孔雨露1111111111111111111111111111111111',
+              style: TextStyle(fontSize: 15),
+            ) ,
+            color: Colors.blue,
+            height: 80,),
+        )
+
+        ,
+        Container(child:Text('努力',style: TextStyle(fontSize: 30),)
+          ,color: Colors.yellow,height: 80,),
+
+        Expanded(
+          child: Container(child:Text('坚持',style: TextStyle(fontSize: 60),) ,color: Colors.green,height: 80,),
+        )
+        ,
+
+      ],
     );
   }
 }
